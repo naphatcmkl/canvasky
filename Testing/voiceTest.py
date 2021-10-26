aa269f92f684cf01715ee4938755dcb4cbf19aa9
Thanapat Eurboonyanun <champ_tnt@hotmail.com>
2021-10-26 17:36:07 +0700

    Add gitignore and test speech recognition with function

--- Testing/voiceTest.py
+++ Testing/voiceTest.py
@@ -1,1 +1,27 @@
-print("Hello, World")
+# ถ้าอ่านอยู่ก็จะบอกว่าไฟล์นี้มีไว้ Test Function ต่างๆ เฉยๆ เพื่อเอาไปต่อยอดในงานจริง
+
+# Import Speech Recognition Package
+import speech_recognition as sr
+
+# Declare r as an receiver
+r = sr.Recognizer()
+
+# Let we receive an input from microphone
+with sr.Microphone() as source:
+    print("Speak: ")
+
+    # Listen
+    audio = r.listen(source)
+
+    try:
+        # Translate word into text
+        text = r.recognize_google(audio)
+
+        # If the word we say translate == champ,
+        if text == "champ":
+            # Then print you're here.
+            print("You're here")
+        else:
+            print("Bye bye!")
+    except:
+        print("Sorry, couldn't recognize your voice.")
