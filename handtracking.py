9db770219126358deac71c94ab6031649caa0c2b
naphat_c <63011191@kmitl.ac.th>
2021-10-30 01:22:58 +0700

    nothing

--- handtracking.py
+++ handtracking.py
@@ -73,8 +73,11 @@ while True:
     imgGray = cv2.cvtColor(blackCanvas, cv2.COLOR_BGR2GRAY)
     _, imgInv = cv2.threshold(imgGray, 50, 255, cv2.THRESH_BINARY_INV)
     imgInv = cv2.cvtColor(imgInv,cv2.COLOR_GRAY2BGR)
-    # img = cv2.bitwise_and(img,imgInv)
-    #img = cv2.bitwise_or(img,blackCanvas)
+    print("type img", type(img))
+    print("type imgInv ",type(imgInv))
+    print("type black ",type(blackCanvas))
+    #img = cv2.bitwise_and(img, imgInv)
+    #img = cv2.bitwise_or(img, blackCanvas)
 
     cv2.imshow("Canvas", img)
     cv2.imshow("Black", blackCanvas)
